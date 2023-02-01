2281f26ba05608697cdafba436db2b38ab3f7928
KarthiGovindasamy37 <107383078+KarthiGovindasamy37@users.noreply.github.com>
2023-02-01 12:22:21 +0530

    Navbar updated

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -13,19 +13,19 @@ function Navbar() {
       <div className="collapse navbar-collapse" id="navbarSupportedContent">
         <ul className="navbar-nav ms-auto mb-2 mb-lg-0 ">
           <li className="nav-item">
-            <NavLink to="/" className={`nav-link me-5 ${({isActive})=> isActive ? `text-dark` : `text-white` }`} >Home</NavLink>
+            <NavLink to="/" className={({isActive})=> isActive ? `nav-link me-5 text-dark` : `nav-link me-5 text-white`} >Home</NavLink>
           </li>
           <li className="nav-item">
-            <NavLink to="skills" className={`nav-link me-5 ${({isActive})=> isActive ? `text-dark` : `text-white` }`} >Skills</NavLink>
+            <NavLink to="skills" className={({isActive})=> isActive ? `nav-link me-5 text-dark` : `nav-link me-5 text-white`} >Skills</NavLink>
           </li>
           <li className="nav-item">
-            <NavLink to="projects" className={`nav-link me-5 ${({isActive})=> isActive ? `text-dark` : `text-white` }`}>Projects</NavLink>
+            <NavLink to="projects" className={({isActive})=> isActive ? `nav-link me-5 text-dark` : `nav-link me-5 text-white`}>Projects</NavLink>
           </li>
           <li className="nav-item">
-            <NavLink to="about" className={`nav-link me-5 ${({isActive})=> isActive ? `text-dark` : `text-white` }`} >About</NavLink>
+            <NavLink to="about" className={({isActive})=> isActive ? `nav-link me-5 text-dark` : `nav-link me-5 text-white`} >About</NavLink>
           </li>
           <li className="nav-item">
-            <NavLink to="contact" className={`nav-link me-5 ${({isActive})=> isActive ? `text-dark` : `text-white` }`} >Contact</NavLink>
+            <NavLink to="contact" className={({isActive})=> isActive ? `nav-link me-5 text-dark` : `nav-link me-5 text-white`} >Contact</NavLink>
           </li>
           </ul>
           </div>
