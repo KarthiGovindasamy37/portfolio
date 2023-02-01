881fffc0158eb90a04fc31f726ba50278c76a6f2
KarthiGovindasamy37 <107383078+KarthiGovindasamy37@users.noreply.github.com>
2023-02-01 12:42:53 +0530

    Navbar updated

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -13,7 +13,7 @@ function Navbar() {
       <div className="collapse navbar-collapse" id="navbarSupportedContent">
         <ul className="navbar-nav ms-auto mb-2 mb-lg-0 ">
           <li className="nav-item">
-            <NavLink to="/" className={({isActive})=> isActive ? `nav-link me-5 text-dark` : `nav-link me-5 text-white`} >Home</NavLink>
+            <NavLink to="/" className={({isActive})=> isActive ? `nav-link me-5 text-dark` : `nav-link me-5 text-white`} end>Home</NavLink>
           </li>
           <li className="nav-item">
             <NavLink to="skills" className={({isActive})=> isActive ? `nav-link me-5 text-dark` : `nav-link me-5 text-white`} >Skills</NavLink>
